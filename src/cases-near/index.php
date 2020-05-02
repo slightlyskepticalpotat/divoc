279d36c6de08fed9849bbd4e8a4de7374e85df98
Computer_Genius <31290848+Computer-Genuis@users.noreply.github.com>
2020-05-02 15:53:35 -0400

    yeet

--- src/cases-near/index.php
+++ src/cases-near/index.php
@@ -2,6 +2,5 @@
   $first = 'ree1';
   $second = 'ree2';
   $third = 'ree3';
-  exec("./testing.exe $first $second $third", $out);
-  echo $out;
+  echo exec("./testing $first $second $third");
 ?>
---
+++ src/cases-near/testing.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc,char* argv[]) {
+    cout<<argv[1]<<' '<<argv[2]<<' '<<argv[3]<<'\n';
+}
