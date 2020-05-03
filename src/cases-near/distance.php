435dec07db768d89ceb53ddb930ed83cd4271341
Computer_Genius <31290848+Computer-Genuis@users.noreply.github.com>
2020-05-02 20:59:36 -0400

    re

--- src/cases-near/distance.php
+++ src/cases-near/distance.php
@@ -2,17 +2,20 @@
   ini_set("allow_url_fopen", 1);
   ini_set('user_agent', 'Hackathon Hack');
   $first = $_POST['address'];
+  $second = $_POST['radius'];
+  $third = "placeholder";
+  // Php trickery
   $address = file_get_contents("https://nominatim.openstreetmap.org/search/" . rawurlencode($first) . "/?format=json&limit=1");
   $decoded = json_decode($address, true);
-
   $lat = $decoded[0]["lat"];
   $lon = $decoded[0]["lon"];
-  echo $lat;
+
+  echo "Address: " . $first;
   echo "<br>";
-  echo $lon;
+  echo "Latitude:" . $lat;
+  echo "<br>";
+  echo "Longitude:" . lon;
   echo '<hr>';
 
-  $second = $_POST['radius'];
-  $third = $_POST['arg3'];
   echo exec("./testing $first $second $third");
 ?>
